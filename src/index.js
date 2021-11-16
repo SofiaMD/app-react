ca3867300c26c0a0b9db97016dfd3297e96783b1
= <=>
2021-11-15 18:49:34 -0600

    Dia 15

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Que? y donde?
+// <p>Contenido</p>
+const p = React.createElement('p',{className: 'id1'}, 'Contenido')
+const p1 = <p className= "id1"><span>Hola Mundo!</span></p>
+const p2 = () => <p>Hola Mundo !</p>    //Crear js dentro de una funcion
+const Div = <div></div>
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  <p2>Hello World!</p2>,
   document.getElementById('root')
 );
 
